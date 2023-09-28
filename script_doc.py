f598c63866e2410907f0551f1ed5ca0608b81d74
kambdan <48095210+kambdan@users.noreply.github.com>
2023-09-28 16:29:27 -0500

    Add soporte para dart

--- script_doc.py
+++ script_doc.py
@@ -1,54 +1,54 @@
 import re
 import argparse
+import os
 
-COMMENTS_STYLES = {
-    '.js': r'(?://[/]?)([^\n]*)',
-    '.cpp': r'(?://[/]?)([^\n]*)',
-    '.py': r'(?://|#)([^\n]*)',
-    '.sql': r'(?://|--)([^\n]*)',
-    '.dart': r'(?://[/]?)([^\n]*)'
-}
+PATTERN_DESCS_FUNC = r"(?:class\s+(\w+))?" + \
+                     r"(?:/\*\s*\*([^*]*?)\*/)?" + \
+                     r"(?:///\s*([^\n]+))??" + \
+                     r"(?://\s([^\n]+)?)?" + \
+                     r"((?:Future|void|String|int|double|bool|var|final)[\w\s\<\>\[\]]+\s*\([^)]*\)\s*async?)?"
 
-def extract_comments(filename, comment_style):
-    comments_list = []
+def extract_comments_and_functions(filename):
     with open(filename, 'r') as file:
         content = file.read()
-        pattern = re.compile(comment_style)
-        comments = pattern.findall(content)
-        
-        for comment in comments:
-            stripped_comment = comment.strip()
-            if stripped_comment:
-                comments_list.append(stripped_comment)
-    return comments_list
+        matches = re.findall(PATTERN_DESCS_FUNC, content)
+    return matches
 
-def to_markdown(comments, output_filename):
+def to_markdown(matches, output_filename):
     with open(output_filename, 'w') as md_file:
-        for comment in comments:
-            if comment.startswith("///"):
-                md_file.write(f"## {comment[3:].strip()}\n")
-            else:
-                md_file.write(f"* {comment[2:].strip()}\n")
+        for match in matches:
+            class_name, general_desc, detail_desc, inout_desc, func = match
+            
+            if class_name:
+                md_file.write(f"# class {class_name.strip()}\n\n")
+            if general_desc:
+                md_file.write(f"**{general_desc.strip()}**\n\n")
+            if detail_desc:
+                md_file.write(f"{detail_desc.strip()}\n\n")
+            if inout_desc:
+                md_file.write(f"{inout_desc.strip()}\n\n")
+            if func.strip():
+                md_file.write(f"```dart\n{func.strip()}\n```\n")
+                md_file.write("---\n\n")
 
 def main():
-    parser = argparse.ArgumentParser(description="Extrae comentarios de un archivo y genera un Markdown.")
-    parser.add_argument("filename", help="Nombre del archivo de código a analizar.")
+    parser = argparse.ArgumentParser(description="Extrae comentarios y funciones en Dart y genera un Markdown.")
+    parser.add_argument("filename", help="Nombre del archivo Dart a analizar.")
     args = parser.parse_args()
 
-    output_filename = 'documentation.md'
+    matches = extract_comments_and_functions(args.filename)
+    
+    # Cambio aquí: usa el nombre base del archivo y agrega la extensión .md
+    base_name = os.path.splitext(args.filename)[0]
+    output_filename = f"{base_name}.md"
+    
+    to_markdown(matches, output_filename)
 
-    extension = "." + args.filename.split('.')[-1]
-    comment_style = COMMENTS_STYLES.get(extension)
-
-    if not comment_style:
-        print(f"Error: No se reconoce la extensión {extension}.")
-        return
-
-    comments = extract_comments(args.filename, comment_style)
-    to_markdown(comments, output_filename)
-
-    print(f"Documentación generada en {output_filename}")
+    print(f"Documentación generada en {output_filename}.")
     print("Documentación generada exitosamente!")
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
